8529aff69fe7e78fd47b6d8e9a83a38fb8dbdf01
Maxim Schiffmann <42582464+maximschiffmann@users.noreply.github.com>
2020-12-21 12:07:06 +0100

    10

--- Vorlesung17/src/BoyerMoore/Boyermoore.java
+++ Vorlesung17/src/BoyerMoore/Boyermoore.java
@@ -4,6 +4,7 @@ public class Boyermoore {
 	public int boyermooresearch(String text, int n, String muster, int m) {
 		int i, j, sizeOfAlphabet = 28;
 		int[] shift = new int[sizeOfAlphabet];
+		boolean go = false;
 
 		for (i = 0; i < sizeOfAlphabet; i++) {
 			shift[i] = m;
@@ -11,30 +12,46 @@ public class Boyermoore {
 		for (i = 0; i < m; i++) {
 			shift[getIndex(muster.charAt(i))] = m - i - 1;
 		}
-//aaa
-//bbb
+
+		for (int c = 0; c < shift.length; c++) {
+			System.out.println(c + ": " + shift[c]);
+		}
+
+		System.out.println(text);
 		int count = 0;
 		j = i = m - 1;
 // j = i = 2, n = 15
 		while (i < n) {
+			if (go) {
+				for (int k = 0; k < i-j; k++) {
+					System.out.print("*");
+				}
+			}
+			System.out.println(muster);
 			if (text.charAt(i) == muster.charAt(j)) {
+
 				if (j == 0) {
 					count++;
 					i += m;
 					j = m - 1;
+					System.out.println();
 				} else {
 					i--;
 					j--;
 				}
 			} else {
+
 				if (m - j > shift[getIndex(text.charAt(i))]) {
+					System.out.println("m: " + m + " j: " + j);
 					i = i + m - j;
 				} else {
-					System.out.println(shift[getIndex(text.charAt(i))]);
+					// System.out.println(shift[getIndex(text.charAt(i))]);
 					i = i + shift[getIndex(text.charAt(i))];
+					// System.out.println("else i: " + i);
 				}
 				j = m - 1;
 			}
+			go = true;
 		}
 		return count;
 	}
--- Vorlesung17/src/BoyerMoore/Programm.java
+++ Vorlesung17/src/BoyerMoore/Programm.java
@@ -4,8 +4,8 @@ public class Programm {
 
 	public static void main(String[] args) {
 		Boyermoore search = new Boyermoore();
-		String text = "wow wow wiw wow";
-		String muster = "wow";
+		String text = "EIN MUSTER P WIRD IN EINEM GEGEBENEN TEXT GESUCHT";
+		String muster = "KEIN";
 		
 		int pos =search.boyermooresearch(text, text.length(), muster, muster.length());
 		System.out.println(pos);
